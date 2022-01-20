a0a6439cb9abcb2d929402e0fe5ecf387cabfd42
reidjJava <sp1.ke@mail.ru>
2022-01-20 19:03:52 +0300

    Update LobbyHandler.kt

--- lobby/src/main/kotlin/me/reidj/bridgebuilders/LobbyHandler.kt
+++ lobby/src/main/kotlin/me/reidj/bridgebuilders/LobbyHandler.kt
@@ -9,6 +9,7 @@ import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPhysicsEvent
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.FoodLevelChangeEvent
+import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
 import org.spigotmc.event.player.PlayerSpawnLocationEvent
@@ -67,6 +68,11 @@ object LobbyHandler : Listener {
 
     @EventHandler
     fun BlockBreakEvent.handle() {
+        cancel = true
+    }
+
+    @EventHandler
+    fun PlayerDropItemEvent.handle() {
         cancel = true
     }
 }
