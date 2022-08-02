20f4d846ba9bdcc5b80014dc52fc8dac4125eff5
reidjJava <sp1.ke@mail.ru>
2022-08-02 20:31:11 +0300

    Добавлены новые префиксы

--- commons/src/main/kotlin/me/reidj/bridgebuilders/donate/Rare.kt
+++ commons/src/main/kotlin/me/reidj/bridgebuilders/donate/Rare.kt
@@ -5,7 +5,9 @@ enum class Rare(val title: String, private val color: String) {
     COMMON("Обычный", "§a"),
     RARE("Редкий", "§9"),
     EPIC("Эпический", "§5"),
-    LEGENDARY("Легендарный", "§6"),;
+    LEGENDARY("Легендарный", "§6"),
+    UNIQUE("Уникальный", "§d"),
+    ;
 
     fun with(content: String): String {
         return "${getColored()} §7$content"
