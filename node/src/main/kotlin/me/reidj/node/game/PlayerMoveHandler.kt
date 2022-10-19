e32e3d4a34b8c4e1ae6aee805ab29725655a585b
reidjJava <sp1.ke@mail.ru>
2022-10-19 19:10:39 +0300

    Теперь мост не должен откидывать игрока, если мост достроен

--- node/src/main/kotlin/me/reidj/node/game/PlayerMoveHandler.kt
+++ node/src/main/kotlin/me/reidj/node/game/PlayerMoveHandler.kt
@@ -35,8 +35,11 @@ class PlayerMoveHandler(private val game: BridgeGame) : Listener {
             return
 
         // Если мост не достроен откидывать от него игрока
-        teams.filter { it.getCountBlocksTeam(game.mapType) && it.bridge.end.distanceSquared(location) < 29 * 12 && !player.isSpectator() }
-            .forEach { player.velocity = it.spawn.toVector().subtract(location.toVector()).normalize() }
+        teams.forEach { team ->
+            if (team.getCountBlocksTeam(game.mapType) && team.bridge.end.distanceSquared(location) < 29 * 12 && !player.isSpectator()) {
+                player.velocity = team.spawn.toVector().subtract(location.toVector()).normalize()
+            }
+        }
 
         // Телепортация на вражескую базу
         if (location.block.getRelative(BlockFace.DOWN).type == Material.SEA_LANTERN) {
